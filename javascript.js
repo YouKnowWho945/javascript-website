1f157cbd1a683dc23ecd491f6af6cd94ef050f5c
Jennifer Blalock <cadenblalock@gmail.com>
2024-11-19 13:02:09 -0500

    commited

--- javascript.js
+++ javascript.js
@@ -1,4 +1,17 @@
 function onPress(imputName) {
-    alert("Hello, " + imputName + "!");
-    document.getElementById("output").innerHTML = "Now I am in control";
+    const name = "john"
+    let age = 10
+    age = 11
+    age = add()
+
+  myAlert(age);
+  document.getElementById("output").innerHTML = "Now I am in control! Mwahahaha!!!";
 }
+function add() {
+    return 1 + 1;
+}
+
+function myAlert(imputName) {
+    alert("You are a fool, " + imputName + "!");
+}
+
